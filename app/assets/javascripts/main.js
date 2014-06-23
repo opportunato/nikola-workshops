0830db8f22dda61d03d5ea73403ab4bc2fbbdea3
panddr <panddr@gmail.com>
2014-06-23 15:44:50 +0400

    last changes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -39,6 +39,30 @@ $('.arrowDown').on('click', function(e) {
   }, 400);
 });
 
+$('.aboutButton').on('click', function(e) {
+  return $('html, body').animate({
+    scrollTop: $(".intro").offset().top
+  }, 400);
+});
+
+$('.workshopsButton').on('click', function(e) {
+  return $('html, body').animate({
+    scrollTop: $(".fullPage").offset().top - 110
+  }, 400);
+});
+
+$('.venueButton').on('click', function(e) {
+  return $('html, body').animate({
+    scrollTop: $(".kazarma").offset().top
+  }, 400);
+});
+
+$('.directionButton').on('click', function(e) {
+  return $('html, body').animate({
+    scrollTop: $(".footer").offset().top
+  }, 400);
+});
+
 // grab an element
 var myElement = document.querySelector("header");
 // construct an instance of Headroom, passing the element
