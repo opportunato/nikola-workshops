d54cd917a302a501da920c811c55e8ba40ebc9b5
panddr <panddr@gmail.com>
2014-06-25 01:50:28 +0400

    homepage is ready

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,32 +5,14 @@ $(function() {
 
   $(".intro").height(vph);
   $(".kazarma").height(vph);
-  // $(".columns").height(vph);
-  // $(".fullPage").css({
-  //   marginTop: vph
-  // });
 
   $(window).resize(function() {
     vph = $(window).height();
     $(".intro").height(vph);
     $(".kazarma").height(vph);
-    // $(".columns").height(vph);
-    // if ($(window).width() > 1) {
-    //   return $(".fullPage").css({
-    //     marginTop: vph
-    //   });
-    // } else {
-    //   return $(".fullPage").css({
-    //     marginTop: 0
-    //   });
-    // }
   });
 
   $('.menuButton').on('click', function(e) {
-    $('header').toggleClass('opened');
-  });
-
-  $('.menu').on('click', function(e) {
     $('header').toggleClass('opened');
   });
 
