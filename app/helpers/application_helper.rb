249f30c23e815896e6769addd7b6a9b9cc8d18ae
opportunato <ryu.gordeyev@gmail.com>
2014-07-12 17:22:59 +0400

    Fixed background images for production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
       content_tag(tag, "", "ng-model" => model, id: model, class: 'form-control')
     end
   end
+
+  def intro_image_url
+    images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg', 'bg5.jpg']
+
+    image_path(images.sample)
+  end
 end
