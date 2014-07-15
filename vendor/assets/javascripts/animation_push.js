9eadcf68b19e6961635547d871ea5d88f4bacb92
panddr <panddr@gmail.com>
2014-07-15 13:36:33 +0400

    animation push js fix, css fixes

--- vendor/assets/javascripts/animation_push.js
+++ vendor/assets/javascripts/animation_push.js
@@ -1,153 +1,133 @@
-(function() {
-  // detect if IE : from http://stackoverflow.com/a/16657946    
-  var ie = (function(){
-    var undef,rv = -1; // Return value assumes failure.
-    var ua = window.navigator.userAgent;
-    var msie = ua.indexOf('MSIE ');
-    var trident = ua.indexOf('Trident/');
+/*jshint browser:true, strict: false*/
+/*global $*/
 
-    if (msie > 0) {
-      // IE 10 or older => return version number
-      rv = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-    } else if (trident > 0) {
-      // IE 11 (or newer) => return version number
-      var rvNum = ua.indexOf('rv:');
-      rv = parseInt(ua.substring(rvNum + 3, ua.indexOf('.', rvNum)), 10);
-    }
+$(function() {
+// detect if IE : from http://stackoverflow.com/a/16657946    
+var ie = (function() {
+  var rv = -1; // Return value assumes failure.
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf('MSIE ');
+  var trident = ua.indexOf('Trident/');
 
-    return ((rv > -1) ? rv : undef);
-  }());
-
-  var keys = [32, 37, 38, 39, 40], wheelIter = 0;
-
-  function preventDefault(e) {
-    e = e || window.event;
-    if (e.preventDefault)
-    e.preventDefault();
-    e.returnValue = false;  
+  if (msie > 0) {
+  // IE 10 or older => return version number
+  rv = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  } else if (trident > 0) {
+  // IE 11 (or newer) => return version number
+  var rvNum = ua.indexOf('rv:');
+  rv = parseInt(ua.substring(rvNum + 3, ua.indexOf('.', rvNum)), 10);
   }
 
-  function keydown(e) {
-    for (var i = keys.length; i--;) {
-      if (e.keyCode === keys[i]) {
-        preventDefault(e);
-        return;
-      }
+  return ((rv > -1) ? rv : undefined);
+}());
+
+var keys = [32, 37, 38, 39, 40], 
+  wheelIter = 0,
+  oriental,
+  $w = $(window);
+
+function keydown(e) {
+  for (var i = keys.length; i--;) {
+    if (e.keyCode === keys[i]) {
+      e.preventDefault();
+      return;
     }
   }
+}
 
-  function touchmove(e) {
-    preventDefault(e);
-  }
+function touchmove(e) {
+  e.preventDefault();
+}
 
-  function disable_scroll() {
-    window.onmousewheel = document.onmousewheel;
-    document.onkeydown = keydown;
-    document.body.ontouchmove = touchmove;
-  }
+function orientationPervert () {
+  clearTimeout(oriental);
+  oriental = setTimeout(function() {
+    clearTimeout(oriental);
+    oriental = undefined;
+  }, 100);
+}
 
-  function enable_scroll() {
-    window.onmousewheel = document.onmousewheel = document.onkeydown = document.body.ontouchmove = null;
-  }
+function disable_scroll() {
+  window.onmousewheel = document.onmousewheel;
+  document.onkeydown = keydown;
+  document.body.ontouchmove = touchmove;
+}
 
-  var docElem = window.document.documentElement,
-    scrollVal,
-    isRevealed = false, 
-    noscroll, 
-    isAnimating,
-    touchDevices = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
-    container = document.getElementById( 'container' ),
-    trigger = container.querySelector( '.trigger' );
+function enable_scroll() {
+  window.onmousewheel = document.onmousewheel = document.onkeydown = document.body.ontouchmove = null;
+  orientationPervert();
+}
 
-  function scrollY() {
-    return window.pageYOffset || docElem.scrollTop;
-  }
-  
-  function scrollPage() {
-    scrollVal = scrollY();
-    
-    if( noscroll && !ie ) {
-      if( scrollVal < 0 ) return false;
-      // keep it that way
+var docElem = window.document.documentElement,
+  scrollVal,
+  isRevealed = false, 
+  noscroll, 
+  isAnimating,
+  touchDevices = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+  $container = $( '#container' ),
+  $trigger = $container.find( '.trigger' );
+
+function scrollPage(e) {
+  scrollVal = $w.scrollTop();
+
+  if( noscroll && !ie ) {
+    if( scrollVal >= 0 ) {
       window.scrollTo( 0, 0 );
     }
-
-    if( classie.has( container, 'notrans' ) ) {
-      classie.remove( container, 'notrans' );
-      return false;
-    }
-
-    if( isAnimating ) {
-      return false;
-    }
-    if( scrollVal <= 0 && isRevealed ) {
-      toggle(0);
-    } else if( scrollVal > 0 && !isRevealed ){
-      toggle(1);
-    }
+  }
+    
+  if( $container.hasClass('notrans' ) ) {
+    $container.removeClass('notrans');
+    e.preventDefault();
+  } else if( isAnimating || oriental) {
+    e.preventDefault();
+  } else if( scrollVal <= 0 && isRevealed) {
+    toggle(0);
+  } else if( scrollVal > 0 && !isRevealed){
+    toggle(1);
   }
 
-  function toggle( reveal ) {
-    isAnimating = true;
-    
-    if( reveal ) {
-      classie.add( container, 'modify' );
-    }
-    else {
-      noscroll = true;
-      disable_scroll();
-      classie.remove( container, 'modify' );
-    }
+}
 
-    // simulating the end of the transition:
-    setTimeout( function() {
-      isRevealed = !isRevealed;
-      isAnimating = false;
-      if( reveal ) {
-        noscroll = false;
-        enable_scroll();
-      }
-    }, 1200 );
-  }
+function toggle( reveal ) {
+  isAnimating = true;
 
-  if (trigger) {
-    // refreshing the page...
-    var pageScroll = scrollY();
-    noscroll = pageScroll === 0;
-    
+  if( reveal ) {
+    $container.addClass('modify');
+  } else {
+    noscroll = true;
     disable_scroll();
-    
-    if( pageScroll ) {
+    $container.removeClass('modify');
+  }
+  // simulating the end of the transition:
+  setTimeout( function() {
+    isAnimating = false;
+    if( reveal ) {
       isRevealed = true;
-      classie.add( container, 'notrans' );
-      classie.add( container, 'modify' );
-    }
-    
-
-    if( touchDevices ) {
-      $(window).on("orientationchange", function(event){
-        if( !isRevealed ) {
-          $(document).scrollTop(0);
-          window.removeEventListener( 'scroll', scrollPage );
-        } else if( isRevealed ) {
-          window.removeEventListener( 'scroll', scrollPage );
-          if (isRevealed) {
-            window.addEventListener( 'touchmove', function() {
-              window.addEventListener( 'scroll', scrollPage );
-            });
-          }
-        }
-      });
+      noscroll = false;
+      enable_scroll();
     } else {
-      window.addEventListener( 'scroll', scrollPage );
+      isRevealed = false;
     }
-    trigger.addEventListener( 'click', function() { 
-      toggle( 'reveal' );
-      if( touchDevices ) {
-        setTimeout( function() {
-          window.addEventListener( 'scroll', scrollPage );
-        }, 1200 );
-      }
-    } );
-  } 
-})();
+  }, 1800 );
+}
+
+// refreshing the page...
+var pageScroll = $w.scrollTop();
+noscroll = pageScroll === 0;
+
+disable_scroll();
+
+if( pageScroll ) {
+  isRevealed = true;
+  $container.addClass('modify notrans');
+}
+$w
+  .on("scroll", scrollPage )
+  .on("orientationchange", orientationPervert);
+
+$trigger.on( 'click', function() { 
+  toggle( 'reveal' );
+});
+
+});
