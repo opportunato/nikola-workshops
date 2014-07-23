07e2e36414ed7031938b8852fe08722f0dda9351
opportunato <ryu.gordeyev@gmail.com>
2014-07-23 09:41:51 +0200

    Removed authentication for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate
 
   after_filter :set_csrf_cookie_for_ng
 
@@ -18,17 +17,11 @@ protected
     super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
   end
 
-  def authenticate
-    if Rails.env.development?
-      login = authenticate_or_request_with_http_basic do |username, password|
-        username == ENV['USERNAME'] && Digest::SHA1.hexdigest(password) == ENV['PASSWORD_HASH']
-      end
-    end
-  end
-
   def admin_login
-    if login = authenticate_or_request_with_http_basic { |u, p| u == ENV['ADMIN_USERNAME'] && Digest::SHA1.hexdigest(p) == ENV['ADMIN_PASSWORD_HASH'] }
-      session[:admin] = true
+    if RAILS.env.production?
+      if login = authenticate_or_request_with_http_basic { |u, p| u == ENV['ADMIN_USERNAME'] && Digest::SHA1.hexdigest(p) == ENV['ADMIN_PASSWORD_HASH'] }
+        session[:admin] = true
+      end
     end
   end
 end
